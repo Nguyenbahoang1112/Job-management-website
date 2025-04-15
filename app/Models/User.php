715de2e9a516435c6720075f16946be60382fcaf
vuongchienn <vuongtatchien21122004@gmail.com>
2025-04-15 15:32:47 +0700

    add relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,43 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
+
+
+    public const ROLE_ADMIN = 1;
+    public const ROLE_USER = 0;
+
+    public const STATUS_ACTIVE = 1;
+    public const STATUS_BANNED = 0;
+
+    public static array $roleLabels = [
+        self::ROLE_ADMIN => 'Admin',
+        self::ROLE_USER => 'Người dùng'
+    ];
+    
+    public static array $statusLables = [
+        self::STATUS_ACTIVE => 'Đang hoạt động',
+        self::STATUS_BANNED => 'Bị khóa'
+    ];
+
+    public function getRoleLabel(){
+        return self::$roleLabels[$this->role];
+    }
+
+    public function getStatusLabel(){
+        return self::$statusLables[$this->status];
+    }
+
+    public function notes(){
+        return $this->hasMany(Note::class);
+    }
+
+
     protected $fillable = [
         'name',
         'email',
         'password',
         'role',
+        'email_verified_at'
     ];
 
     /**
