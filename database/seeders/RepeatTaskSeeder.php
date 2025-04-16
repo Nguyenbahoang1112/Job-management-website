8f4106a637b0e36c9b8898165cd600d3e79967c0
vuongchienn <vuongtatchien21122004@gmail.com>
2025-04-16 09:26:55 +0700

    update file seed

--- database/seeders/RepeatTaskSeeder.php
+++ database/seeders/RepeatTaskSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
-use App\Models\RepeateTask;
+use App\Models\RepeatTask;
 use App\Models\Task;
 class RepeatTaskSeeder extends Seeder
 {
@@ -19,7 +19,7 @@ class RepeatTaskSeeder extends Seeder
         $taskIds = Task::pluck('id');
 
         for($i = 0;$i< 100;$i++){
-            RepeateTask::create([
+            RepeatTask::create([
                 'task_id' => $taskIds->random(),
                 'repeat_type' => rand(1,3),
                 'repeat_interval' => 3,
